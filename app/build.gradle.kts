1d1fc46dd54fcd6d434614dc7f31d31a116624ea
Ivan(kibatora) <zaranet1999@gmail.com>
2024-12-17 21:33:54 +0700

    удаление кнопки за бесполозностью. добавление карты гугл

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
 
 dependencies {
 
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation ("androidx.fragment:fragment-ktx:1.8.5")
     implementation(libs.play.location)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/com/example/vis/MainActivity.kt
+++ app/src/main/java/com/example/vis/MainActivity.kt
@@ -1,32 +1,51 @@
 package com.example.vis
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.Gravity
+import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
 
-class MainActivity : Activity() {
+class MainActivity : Activity(), OnMapReadyCallback {
+
 
     private lateinit var tvLocation: TextView
     private lateinit var tvSignalStrength: TextView
-    private lateinit var btnUpdate: Button
-
+    private var mapFragment: MapFragment? = null
+    private var googleMap: GoogleMap? = null
     private val handler = Handler(Looper.getMainLooper())
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
     private val updateRunnable = object : Runnable {
         override fun run() {
             updateLocationData()
             handler.postDelayed(this, 30000)
         }
     }
+
+
+
 
     private companion object {
         const val LOCATION_PERMISSION_REQUEST_CODE = 1000
@@ -38,11 +57,9 @@ class MainActivity : Activity() {
         val linearLayout = LinearLayout(this).apply {
             orientation = LinearLayout.VERTICAL
             layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            ).apply {
-                gravity = Gravity.CENTER
-            }
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+            )
             setPadding(16, 16, 16, 16)
 
             tvLocation = TextView(this@MainActivity).apply {
@@ -55,14 +72,85 @@ class MainActivity : Activity() {
             }
             addView(tvSignalStrength)
 
-            // Кнопка btnUpdate удалена
+            val frameLayout = FrameLayout(this@MainActivity).apply {
+                layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    0
+                ).apply {
+                    weight = 1f
+                }
 
+                id = View.generateViewId()
+            }
+            addView(frameLayout)
+
+            mapFragment = MapFragment.newInstance()
+            fragmentManager.beginTransaction()
+                .replace(frameLayout.id, mapFragment!!)
+                .commit()
+
+            mapFragment?.getMapAsync(this@MainActivity)
+
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
         }
 
         setContentView(linearLayout)
 
         LatLon.initialize(this) // Инициализация LatLon
+
         handler.post(updateRunnable) // Запускаем автоматическое обновление
+
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+        updateLocationData()
+    }
+
+
+
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocationData() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            val locationRequest = LocationRequest.create().apply {
+                interval = 10000
+                fastestInterval = 5000
+                priority = Priority.PRIORITY_HIGH_ACCURACY
+            }
+
+            locationCallback = object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult) {
+                    val location = locationResult.lastLocation
+
+
+                    if (location != null) {
+                        val latitude = location.latitude
+                        val longitude = location.longitude
+                        tvLocation.text = "Широта: $latitude\nДолгота: $longitude"
+
+
+                        googleMap?.let {
+                            val latLng = LatLng(latitude, longitude)
+                            it.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+                            it.isMyLocationEnabled = true
+
+
+                        }
+
+                        Power.getSignalStrength(this@MainActivity) { strength ->
+                            tvSignalStrength.text = "Мощность сигнала: $strength"
+                        }
+                    }
+
+                }
+
+            }
+
+
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+        }
     }
 
 
@@ -72,28 +160,16 @@ class MainActivity : Activity() {
         handler.removeCallbacks(updateRunnable)
     }
 
-
-    private fun updateLocationData() {
-        LatLon.getLocation(this) { location ->
-            if (location != null) {
-                val latitude = location.latitude
-                val longitude = location.longitude
-                tvLocation.text = "Широта: $latitude\nДолгота: $longitude"
-
-                Power.getSignalStrength(this) { strength ->
-                    tvSignalStrength.text = "Мощность сигнала: $strength"
-                }
-            } else {
-                tvLocation.text = "Местоположение недоступно"
-            }
-        }
-    }
     override fun onDestroy() {
         super.onDestroy()
         LatLon.stopLocationUpdates(this)
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
