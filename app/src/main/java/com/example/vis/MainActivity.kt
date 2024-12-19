a553667c4539162b92101d5ab87a754a81ea799a
Ivan(kibatora) <zaranet1999@gmail.com>
2024-12-19 23:39:22 +0700

    починка линий между точками handover, также загрузка из файла на карту этих же меток(нужна проверка.)

--- app/src/main/java/com/example/vis/MainActivity.kt
+++ app/src/main/java/com/example/vis/MainActivity.kt
@@ -13,7 +13,10 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.location.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapFragment
@@ -33,7 +36,6 @@ import java.util.Locale
 
 class MainActivity : Activity(), OnMapReadyCallback {
 
-
     private lateinit var tvLocation: TextView
     private lateinit var tvSignalStrength: TextView
     private var mapFragment: MapFragment? = null
@@ -42,10 +44,9 @@ class MainActivity : Activity(), OnMapReadyCallback {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationCallback: LocationCallback
     private val cellInfoList = mutableListOf<CellInfo>()
+
     private var cameraPosition: CameraPosition? = null
-
     private var previousCellInfo: CellInfo? = null
-
 
     private val updateRunnable = object : Runnable {
         override fun run() {
@@ -53,17 +54,12 @@ class MainActivity : Activity(), OnMapReadyCallback {
             handler.postDelayed(this, 30000)
         }
     }
-
-
-
     private companion object {
         const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
         val linearLayout = LinearLayout(this).apply {
             orientation = LinearLayout.VERTICAL
@@ -73,18 +69,15 @@ class MainActivity : Activity(), OnMapReadyCallback {
             )
             setPadding(16, 16, 16, 16)
 
-
             tvLocation = TextView(this@MainActivity).apply {
                 textSize = 18f
             }
             addView(tvLocation)
 
-
             tvSignalStrength = TextView(this@MainActivity).apply {
                 textSize = 18f
             }
             addView(tvSignalStrength)
-
 
             val frameLayout = FrameLayout(this@MainActivity).apply {
                 layoutParams = LinearLayout.LayoutParams(
@@ -97,19 +90,17 @@ class MainActivity : Activity(), OnMapReadyCallback {
             }
             addView(frameLayout)
 
-
             mapFragment = MapFragment.newInstance()
             fragmentManager.beginTransaction()
                 .replace(frameLayout.id, mapFragment!!)
                 .commit()
 
-
             mapFragment?.getMapAsync(this@MainActivity)
+
             fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
 
 
         }
-
 
         setContentView(linearLayout)
         LatLon.initialize(this)
@@ -117,17 +108,14 @@ class MainActivity : Activity(), OnMapReadyCallback {
 
     }
 
-
     override fun onMapReady(googleMap: GoogleMap) {
         this.googleMap = googleMap
-
+        loadDataFromFile()
     }
-
 
     @SuppressLint("MissingPermission")
     private fun updateData() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-
             LatLon.getLocation(this) { location ->
 
                 if (location != null) {
@@ -136,16 +124,8 @@ class MainActivity : Activity(), OnMapReadyCallback {
                     val longitude = location.longitude
                     val latLng = LatLng(latitude, longitude)
 
-                    var strength : Int?
-                    var rsrp: Int?
-                    var pci : Int?
-
                     tvLocation.text = "Широта: $latitude\nДолгота: $longitude"
-
-                    val triple = Power.getSignalStrength(this@MainActivity) // Получаем triple
-                    strength = triple.first
-                    rsrp = triple.second
-                    pci = triple.third
+                    val (strength, rsrp, pci) = Power.getSignalStrength(this@MainActivity)
 
 
                     val rsrpValue = rsrp ?: 0
@@ -153,9 +133,10 @@ class MainActivity : Activity(), OnMapReadyCallback {
 
                     tvSignalStrength.text = "Мощность сигнала: $strength, RSRP: $rsrpValue, PCI: $pci"
 
-
                     googleMap?.let { googleMap ->
-                        if (previousCellInfo != null && currentCellInfo.pci != previousCellInfo?.pci) {
+
+                        if(cellInfoList.isNotEmpty() && previousCellInfo != null && currentCellInfo.pci != previousCellInfo?.pci) {
+
                             val previousLatLng = previousCellInfo!!.location
 
                             googleMap.addPolyline(
@@ -164,15 +145,14 @@ class MainActivity : Activity(), OnMapReadyCallback {
                                     .width(5f)
                                     .color(Color.BLUE)
                             )
-
-
                             currentCellInfo.marker = googleMap.addMarker(
                                 MarkerOptions()
                                     .position(latLng)
                                     .title("PCI: $pci, RSRP: $rsrpValue (Handover)")
                             )
-                        }
-                        else if (cellInfoList.isEmpty()){
+
+
+                        } else if (cellInfoList.isEmpty()) {
                             currentCellInfo.marker = googleMap.addMarker(
                                 MarkerOptions()
                                     .position(latLng)
@@ -188,13 +168,13 @@ class MainActivity : Activity(), OnMapReadyCallback {
                             googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition!!))
                         }
 
-
                         googleMap.isMyLocationEnabled = true
                     }
 
 
                     previousCellInfo = currentCellInfo
                     cellInfoList.add(currentCellInfo)
+
 
                     val handover = if (previousCellInfo != null && currentCellInfo.pci != previousCellInfo?.pci) "true" else "false"
 
@@ -208,59 +188,98 @@ class MainActivity : Activity(), OnMapReadyCallback {
 
                     saveLogToFile(logMessage)
 
-                }
-                else {
+
+                } else {
+
                     tvLocation.text = "Местоположение недоступно"
                 }
             }
-
-
-
-        } else {
-
+        }
+        else {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                 LOCATION_PERMISSION_REQUEST_CODE
             )
-
-
         }
 
     }
+
 
     private fun getCurrentTimestamp(): String {
         val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
         return sdf.format(Date())
     }
 
-
-
     private fun saveLogToFile(logMessage: String) {
         try {
             val file = File(getExternalFilesDir(null), "location_log.csv")
-            val fos = FileOutputStream(file, true) // true для добавления в конец файла
+            val fos = FileOutputStream(file, true)
             fos.bufferedWriter().use { it.write("$logMessage\n") }
             fos.close()
-        } catch (e: IOException) {
+        }
+        catch (e: IOException) {
             e.printStackTrace()
-
         }
     }
 
+    private fun loadDataFromFile() {
+
+        val file = File(getExternalFilesDir(null), "location_log.csv")
+        if (file.exists()) {
+            try {
+                val lines = file.readLines()
+                googleMap?.clear() //  Очищаем карту перед загрузкой старых данных
+                for (line in lines) {
+                    val parts = line.split(",").map { it.trim() }
+                    if (parts.size == 7) {
+                        val latitude = parts[1].substringAfter(":").trim().toDoubleOrNull() ?: continue
+                        val longitude = parts[2].substringAfter(":").trim().toDoubleOrNull() ?: continue
+                        val pci = parts[5].substringAfter(":").trim().toIntOrNull()
+                        val rsrp = parts[4].substringAfter(":").trim().toIntOrNull() ?: 0
+                        val handover = parts[6].substringAfter(":").trim()
+                        val latLng = LatLng(latitude,longitude)
+                        val currentCellInfo = CellInfo(pci, rsrp, latLng)
+                        cellInfoList.add(currentCellInfo)
+                        googleMap?.let{ googleMap->
+                            currentCellInfo.marker = googleMap.addMarker(
+                                MarkerOptions()
+                                    .position(latLng)
+                                    .title("PCI: $pci, RSRP: $rsrp ${if(handover=="true") "(Handover)" else ""}" )
+
+                            )
+                            if(previousCellInfo!=null && currentCellInfo.pci != previousCellInfo?.pci )
+                            {
+                                val previousLatLng = previousCellInfo!!.location
+
+                                googleMap.addPolyline(
+                                    PolylineOptions()
+                                        .add(previousLatLng, latLng)
+                                        .width(5f)
+                                        .color(Color.BLUE)
+                                )
+                            }
+                        }
+                        previousCellInfo = currentCellInfo
+                    }
+                }
+            }
+            catch (e: IOException) {
+                e.printStackTrace()
+
+            }
+        }
+    }
 
     override fun onPause() {
         super.onPause()
         handler.removeCallbacks(updateRunnable)
     }
 
-
     override fun onDestroy() {
         super.onDestroy()
         LatLon.stopLocationUpdates(this)
     }
-
-
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
